49b8d9f2c8ce6633c2be018768dccb701328a4b0
Muhammad Naufal <muhammad.naufal@topremit.com>
2025-10-23 19:16:31 +0700

    feat(ui): add necessary base ui component

---
+++ src/components/providers.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Toaster } from './ui/sonner';
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <React.Fragment>
+      <Toaster closeButton position="bottom-left" />
+      {children}
+    </React.Fragment>
+  );
+}
+
+export default Providers;
